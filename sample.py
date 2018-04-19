f3a253e3d59f45cc46ff39c67eff117ac462667f
nikhiljohnjoseph <38380596+nikhiljohnjoseph@users.noreply.github.com>
2018-04-19 12:32:24 +0530

    first modification to the program
    added pie chart and previous label deleting problem resolved.

--- sample.py
+++ sample.py
@@ -2,12 +2,20 @@ from tkinter import *
 import random
 
 i=0
+valueArr=[]
+rollingNo=[]
+count=[0,0,0,0,0,0]
+
 def click():
+    intro.destroy()
     global i;
     i+=1
     diceNumber=random.randint(1,6)
     Label (window, text=diceNumber, bg="black", fg="white", font="none 20 bold", height=5) .grid(row=1, column=0, sticky=N)
-    print("roll Number ",i," is ",diceNumber)
+    valueArr.append(diceNumber)
+    rollingNo.append(i)
+    count[diceNumber-1]=count[diceNumber-1]+1
+    print("Dice rolling Number ",i," is ",diceNumber)
 
         
     
@@ -22,6 +30,11 @@ window.tk.call('wm','iconphoto',window._w,icon)
 
 pic= PhotoImage(file="cool.gif")
 Label (window, text="The Rolling Dice Machine", bg="black", fg="white", font="none 12 bold") .grid(row=0, column=0, sticky=N)
+intro=Label (window, text="Not Yet Rolled", bg="black", fg="white", font="none 12 bold", height=5)
+intro.grid(row=1, column=0, sticky=N)
 Button(window, text="Roll", width=6, command=click) .grid(row=2, column=0, sticky=N)
 window.mainloop()
 
+print(valueArr)
+plt.pie(count,labels=[1,2,3,4,5,6])
+plt.show()
